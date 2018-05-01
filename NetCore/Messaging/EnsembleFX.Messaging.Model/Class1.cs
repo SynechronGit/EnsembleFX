92f070f98e6f6feab9364ebb8214f29806ae8da0
syn-nitinsoni <nitin.soni@synechron.com>
2018-05-01 16:12:57 +0530

    Base structure for Messaging.

---
+++ NetCore/Messaging/EnsembleFX.Messaging.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnsembleFX.Messaging.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ NetCore/Messaging/EnsembleFX.Messaging/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnsembleFX.Messaging
+{
+    public class Class1
+    {
+    }
+}
