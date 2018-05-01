7892914e5f311624606ff850117bd58dbd942dee
syn-tusharmashalkar <tushar.mashalkar@synechron.com>
2018-05-01 16:51:40 +0530

    > Commented the ApiAuthorization Code

--- NetCore/Filters/EnsembleFX.Filters/ApiAuthorization.cs
+++ NetCore/Filters/EnsembleFX.Filters/ApiAuthorization.cs
@@ -1,44 +1,46 @@
-﻿using EnsembleFX.Core.Security;
-using EnsembleFX.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web;
-using System.Web.Http;
-using EnsembleFX.ApiProxy;
+﻿//TODO:: Need to solved ApiAuthorization errors
 
-namespace EnsembleFX.Filters
-{
-    public class ApiAuthorization : AuthorizeAttribute
-    {
-        public string PermissionName { get; set; }
-        public RuntimeApiProxy _runtimeApiProxy { get; set; }
+//using EnsembleFX.Core.Security;
+//using EnsembleFX.Shared;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Web;
+//using System.Web.Http;
+//using EnsembleFX.ApiProxy;
 
-        /// <summary>
-        /// Checks whether the user is authorized to do the requested operation
-        /// </summary>
-        /// <param name="actionContext"> the current Http Request Context</param>
-        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
-        {
-            _runtimeApiProxy = new RuntimeApiProxy();
-            HttpMethod requestType = null;
+//namespace EnsembleFX.Filters
+//{
+//    public class ApiAuthorization : AuthorizeAttribute
+//    {
+//        public string PermissionName { get; set; }
+//        public RuntimeApiProxy _runtimeApiProxy { get; set; }
 
-            if (string.IsNullOrEmpty(this.PermissionName))
-            {
-                base.HandleUnauthorizedRequest(actionContext);
-            }
-            else
-            {
-                if (actionContext.Request != null)
-                    requestType = actionContext.Request.Method;
+//        /// <summary>
+//        /// Checks whether the user is authorized to do the requested operation
+//        /// </summary>
+//        /// <param name="actionContext"> the current Http Request Context</param>
+//        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+//        {
+//            _runtimeApiProxy = new RuntimeApiProxy();
+//            HttpMethod requestType = null;
 
-                if (_runtimeApiProxy.HasRoleAccessCheck(EnsembleContext.Name, this.PermissionName, requestType))
-                    base.OnAuthorization(actionContext);
-                else
-                    base.HandleUnauthorizedRequest(actionContext);
-            }
-        }
+//            if (string.IsNullOrEmpty(this.PermissionName))
+//            {
+//                base.HandleUnauthorizedRequest(actionContext);
+//            }
+//            else
+//            {
+//                if (actionContext.Request != null)
+//                    requestType = actionContext.Request.Method;
 
-    }
-}
+//                if (_runtimeApiProxy.HasRoleAccessCheck(EnsembleContext.Name, this.PermissionName, requestType))
+//                    base.OnAuthorization(actionContext);
+//                else
+//                    base.HandleUnauthorizedRequest(actionContext);
+//            }
+//        }
+
+//    }
+//}
