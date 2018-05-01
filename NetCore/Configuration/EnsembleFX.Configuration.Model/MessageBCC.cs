68520bc1b08010c83bba3b849583c19eafa8ede8
SynSantoshPujar <santosh.pujar@synechron.com>
2018-05-01 20:14:14 +0530

    Added Model for Configuration project

---
+++ NetCore/Configuration/EnsembleFX.Configuration.Model/MessageBCC.cs
@@ -0,0 +1,35 @@
+﻿
+namespace EnsembleFX.Configuration.Model
+{
+    using System;
+    using System.Configuration;
+
+    public class MessageBCC : ConfigurationElement
+    {
+        [ConfigurationProperty("ids")]
+        public String Ids
+        {
+            get
+            {
+                return (String)this["ids"];
+            }
+            set
+            {
+                this["ids"] = value;
+            }
+        }
+
+        [ConfigurationProperty("replaceOrAppend")]
+        public String ReplaceOrAppend
+        {
+            get
+            {
+                return (String)this["replaceOrAppend"];
+            }
+            set
+            {
+                this["replaceOrAppend"] = value;
+            }
+        }
+    }
+}
---
+++ NetCore/Configuration/EnsembleFX.Configuration.Model/MessageElement.cs
@@ -0,0 +1,59 @@
+﻿
+namespace EnsembleFX.Configuration.Model
+{
+    using System.Configuration;
+    public class MessageElement : ConfigurationElement
+    {
+        [ConfigurationProperty("to")]
+        public MessageTo To
+        {
+            get
+            {
+                return (MessageTo)this["to"];
+            }
+            set
+            {
+                this["to"] = value;
+            }
+        }
+
+        [ConfigurationProperty("cc")]
+        public MessageCC CC
+        {
+            get
+            {
+                return (MessageCC)this["cc"];
+            }
+            set
+            {
+                this["cc"] = value;
+            }
+        }
+
+        [ConfigurationProperty("bcc")]
+        public MessageBCC BCC
+        {
+            get
+            {
+                return (MessageBCC)this["bcc"];
+            }
+            set
+            {
+                this["bcc"] = value;
+            }
+        }
+
+        [ConfigurationProperty("subject")]
+        public MessageSubject Subject
+        {
+            get
+            {
+                return (MessageSubject)this["subject"];
+            }
+            set
+            {
+                this["subject"] = value;
+            }
+        }
+    }
+}
