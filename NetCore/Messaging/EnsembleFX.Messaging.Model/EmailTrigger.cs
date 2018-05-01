e7f90e74ac1eec0c33f96afa08bd6b517fecc24f
syn-hiteshbhaipaghadal <38850909+syn-hiteshbhaipaghadal@users.noreply.github.com>
2018-05-01 21:27:41 +0530

    Add necessary class into project

---
+++ NetCore/Messaging/EnsembleFX.Messaging.Model/EmailTrigger.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace EnsembleFX.Messaging.Model
+{
+    public class EmailTrigger : ITrigger, IDocument
+    {
+        public Guid _id { get; set; }
+
+        /// <summary>
+        /// WorkspaceID
+        /// </summary>
+        public Guid WorkspaceID { get; set; }
+
+        /// <summary>
+        /// Imap/Pop Incoming Server Name 
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Email Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Email Password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Server Port
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Is SSL to be used
+        /// </summary>
+        public bool IsSSL { get; set; }
+
+        /// <summary>
+        /// Secure
+        /// </summary>
+        public bool Secure { get; set; }
+
+        /// <summary>
+        /// Skip SSL Validation
+        /// </summary>
+        public bool SkipSSLValidation { get; set; }
+
+        /// <summary>
+        /// Project ID
+        /// </summary>
+        public string ProjectID { get; set; }
+
+        /// <summary>
+        /// Is Trigger Active
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Knowledge Base
+        /// </summary>
+        public bool KnowledgeBase { get; set; }
+
+        /// <summary>
+        /// Trigger Start Date
+        /// </summary>
+        public DateTime? StartDateTime { get; set; }
+
+        /// <summary>
+        /// Trigger Expiry Date
+        /// </summary>
+        public DateTime? ExpiryDate { get; set; }
+
+        /// <summary>
+        /// Trigger status
+        /// </summary>
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// Job Name
+        /// </summary>
+        public string TriggerName { get; set; }
+
+        /// <summary>
+        /// Folder
+        /// </summary>
+        public string Folder { get; set; }
+
+        // added for #18 github Issues
+        public int RefreshHours { set; get; }
+
+        public int RefreshMinutes { set; get; }
+
+        public int RefreshSeconds { set; get; }
+
+        /// <summary>
+        /// Agent Family ID
+        /// </summary>
+        public Guid AgentFamilyId { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+
+}
---
+++ NetCore/Messaging/EnsembleFX.Messaging.Model/IFTTTApplet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Messaging.Model
+{
+    public class IFTTTApplet : IDocument
+    {
+
+        /// <summary>
+        /// Applet Id
+        /// </summary>
+        public Guid _id { get; set; }
+
+        /// <summary>
+        /// Applet Name
+        /// </summary>
+        public string AppletTitle { get; set; }
+
+        /// <summary>
+        /// Applet Description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Trigger Type i.e File, Email or Time trigger
+        /// </summary>
+        public TriggerType TriggerType { get; set; }
+
+        /// <summary>
+        /// Trigger for which this applet is associated with
+        /// </summary>
+        public Guid TriggerId { get; set; }
+
+        /// <summary>
+        /// Workflow
+        /// </summary>
+        public Guid WorkflowId { get; set; }
+
+        /// <summary>
+        /// Published WorkflowDefinition/Version under selected Workflow 
+        /// </summary>
+        public Guid WorkflowDefinitionId { get; set; }
+
+        /// <summary>
+        /// Turn Applet On/Off
+        /// </summary>
+        public bool AppletOnOff { get; set; }
+
+        public List<string> PromotedEnvironments { get; set; }
+
+        public List<Guid> Groups { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
