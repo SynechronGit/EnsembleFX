8e4935439dd04fda4ccfec2ed6e4ab52d6db0cfe
Chirag Vidani <chirag.vidani@synechron.com>
2018-05-01 21:38:06 +0530

    Add required model from StepIn.Entites

---
+++ NetCore/Messaging/EnsembleFX.Messaging.Model/Enums/TaskStatusType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Messaging.Model.Enums
+{
+    public enum TaskStatusType : int
+    {
+        Unknown = 0,
+        Created = 1,
+        Queued = 2,
+        Dequeued = 3,
+        Executing = 4,
+        Aborted = 5,
+        Completed = 6,
+        CompletedWithSuccess = 7,
+        CompletedWithFailure = 8,
+        CompletedPendingRetry = 9
+
+    }
+}
