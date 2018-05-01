d9df1bc21e6d381d6e28dff6c9bd69e92d22195d
syn-hiteshbhaipaghadal <38850909+syn-hiteshbhaipaghadal@users.noreply.github.com>
2018-05-01 21:30:00 +0530

    Add necessary classes into model project

---
+++ NetCore/Messaging/EnsembleFX.Messaging.Model/WorkflowLinkData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Messaging.Model
+{
+    public class WorkflowLinkData
+    {
+        public Guid _id { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public string fromport { get; set; }
+        public string toport { get; set; }
+        public bool visible { get; set; }
+        //public float[] points { get; set; }
+        public double[] points { get; set; }
+
+    }
+}
