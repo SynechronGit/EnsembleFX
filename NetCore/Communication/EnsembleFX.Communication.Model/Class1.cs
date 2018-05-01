3a9b7b9707809420a42c9e7074d31e250b54b959
Prakash Pasalkar <prakash.pasalkar@synechron.com>
2018-05-01 16:44:49 +0530

    TrasportMessage model

--- NetCore/Communication/EnsembleFX.Communication.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EnsembleFX.Communication.Model
-{
-    public class Class1
-    {
-    }
-}
---
+++ NetCore/Communication/EnsembleFX.Communication.Model/TransportMessage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Communication.Model
+{
+    class TransportMessage
+	{
+		#region Public properties
+		/// <summary>
+		/// Blind carbon copy of email message
+		/// </summary>
+		public string Bcc { get; set; }
+
+		/// <summary>
+		/// Content of the message
+		/// </summary>
+		public string Body { get; set; }
+
+		/// <summary>
+		/// Carbon copy of email message
+		/// </summary>
+		public string CC { get; set; }
+
+		/// <summary>
+		/// Sender of the message
+		/// </summary>
+		public string From { get; set; }
+
+		/// <summary>
+		/// Subject of the message
+		/// </summary>
+		public string Subject { get; set; }
+
+		/// <summary>
+		/// Message recipient
+		/// </summary>
+		public string To { get; set; }
+
+		#endregion
+	}
+}
