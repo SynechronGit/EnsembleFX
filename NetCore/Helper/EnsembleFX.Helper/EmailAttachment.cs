463b0273a7c603bf16e22dc5a7de5e43964e9cf3
syn-pradeepdhanrajani <pradeepd@synechron.com>
2018-05-02 23:21:21 -0400

    Migrated to EnsembleFX

---
+++ NetCore/Helper/EnsembleFX.Helper/EmailAttachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnsembleFX.Helper
+{
+    public partial class EmailAttachment
+    {
+        public string AttachmentName { get; set; }
+
+        public MemoryStream AttachmentStream { get; set; }
+
+        public string ContentType { get; set; }
+    }
+}
