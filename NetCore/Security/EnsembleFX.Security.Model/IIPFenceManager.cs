a5e578f5afe088f27b2a37f258bf99280e8ce0d1
syn-saritkumar <38853790+syn-saritkumar@users.noreply.github.com>
2018-05-05 17:15:26 +0530

    Check-in all IPFence Files.

---
+++ NetCore/Security/EnsembleFX.Security.Model/IIPFenceManager.cs
@@ -0,0 +1,22 @@
+﻿using EnsembleFX.Filters;
+using System;
+using System.Collections.Generic;
+
+namespace EnsembleFX.Core.Security.Model
+{
+	/// <summary>
+	/// Interface for IPFence
+	/// </summary>
+	public interface IIPFenceManager
+    {
+        void Init(IEnumerable<IPFenceViewModel> records);
+        IList<IPFence> Get();
+        IPFenceListViewModel GetWhitelist(PagingFiltering pagingFiltering);
+        IPFenceListViewModel GetBlacklist(PagingFiltering pagingFiltering);
+        IPFence Get(Guid ipFenceId);
+        IPFence Add(IPFence IPFence);
+        IPFence Update(IPFence IPFence);
+        bool Delete(Guid ipFenceId);
+        bool Validate(string ipAddress, IEnumerable<IPFenceViewModel> records);
+    }
+}
