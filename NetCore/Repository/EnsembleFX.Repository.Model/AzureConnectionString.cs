c58ef0be40c5e13a2db4e617cb5557ff1bffba9c
Chirag.Gupta <Chirag.Gupta@SYNPUNLT4SY36L2>
2018-05-05 12:46:50 +0530

    Ensemble.Clound

---
+++ NetCore/Repository/EnsembleFX.Repository.Model/AzureConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Repository.Model
+{
+    public class AzureConnectionString
+    {
+        public string ConnectionString { get; set; }
+
+        public string CloudContainer { get; set; }
+    }
+}
