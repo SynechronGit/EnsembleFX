d072c4e222543254bd7764bd897a1b74525b8357
syn-tusharmashalkar <tushar.mashalkar@synechron.com>
2018-05-07 20:29:37 +0530

    > Log and Zipcode Helper added

---
+++ NetCore/Helper/EnsembleFX.Helper/LogHelper.cs
@@ -0,0 +1,24 @@
+﻿using EnsembleFX.Logging;
+using Newtonsoft.Json;
+
+namespace EnsembleFX.Helper
+{
+    public static class LogHelper
+    {
+        public static LogModel GetLogModel(string placeId, string eventName, string message, object request = null, object response = null)
+        {
+            LogModel logModel = new LogModel();
+            logModel.ApplicationIdentifier = placeId;
+            logModel.EventName = eventName;
+            logModel.Message = message;
+
+            if (request != null)
+                logModel.RequestObject = JsonConvert.SerializeObject(request);
+
+            if (response != null)
+                logModel.ResponseObject = JsonConvert.SerializeObject(response);
+
+            return logModel;
+        }
+    }
+}
---
+++ NetCore/Helper/EnsembleFX.Helper/ZipcodeHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Helper
+{
+    public static class ZipcodeHelper
+    {
+        private const string NLCountryCode = "NL";
+        public static string GetFormattedZipCode(string countryCode, string zipcode)
+        {
+            string formattedZipCode = string.Empty;
+            if (countryCode == NLCountryCode)
+            {
+                formattedZipCode = zipcode.Substring(0, 4).Trim();
+            }
+            else
+            {
+                formattedZipCode = zipcode.Trim();
+            }
+            return formattedZipCode;
+        }
+    }
+}
