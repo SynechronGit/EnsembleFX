c5fef3ebcf8573f36a933ca17823e13dbc5ddbd3
Chirag Vidani <chirag.vidani@synechron.com>
2018-05-09 13:53:56 +0530

    Task#3266 Added Pluralization Service in EnsembleFX.sln used in BusinessInsight microserivice InspectionGradeController's method

---
+++ NetCore/Pluralization/EnsembleFX.Pluralization/PluralizationService.cs
@@ -0,0 +1,31 @@
+﻿using PluralizationService;
+using PluralizationService.English;
+using System;
+using System.Globalization;
+
+namespace EnsembleFX.Pluralization
+{
+    public class PluralizationService : IPluralizationService
+    {
+        private readonly IPluralizationApi api;
+        private readonly CultureInfo cultureInfo;
+
+        public PluralizationService()
+        {
+            var builder = new PluralizationApiBuilder();
+            builder.AddEnglishProvider();
+
+            api = builder.Build();
+            cultureInfo = new CultureInfo("en-US");
+        }
+        public string Pluralize(string name)
+        {
+            return api.Pluralize(name, cultureInfo) ?? name;
+        }
+
+        public string Singularize(string name)
+        {
+            return api.Singularize(name, cultureInfo) ?? name;
+        }
+    }
+}
