581f761daf930339b00f618f8a0a657bf9a96151
Chirag Vidani <chirag.vidani@synechron.com>
2018-05-14 15:29:46 +0530

    EnsembleFX -> EnsembleFX.Pluralization to EnsembleFX.Core under Pluralization folder

---
+++ NetCore/Core/EnsembleFX.Core/Pluralization/IPluralizationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnsembleFX.Core.Pluralization
+{
+    public interface IPluralizationService
+    {
+        string Pluralize(string name);
+        string Singularize(string name);
+    }
+}
